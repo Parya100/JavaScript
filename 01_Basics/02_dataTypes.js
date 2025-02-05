108aad66e279be2bc3872b9261963db47667c92c
PRINCE ARYA <181065880+Parya100@users.noreply.github.com>
2025-02-05 15:33:51 +0000

    addnew2

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,4 +1,14 @@
 "uae strict";
-//treat alljs code as newer version
+//treat all js code as newer version
 
-//alert(3 + 3) //we using nodejs,not browser
+//alert(3 + 3) //it use in browser,not in node.js
+console.log(3+3);
+
+let name ="Prince"
+let age = 20
+let Marride =false
+
+//null = asighn value null
+//undefined = use for value not diffene current but it will be diffene
+console.log(typeof "prince");
+console.log(typeof age);
