04996334685411e4128d8ef73187379e4f443bc1
PRINCE ARYA <181065880+Parya100@users.noreply.github.com>
2025-02-05 15:50:40 +0000

    addnew3

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,27 @@
+// let score = "33bh" 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abs"=>NaN
+//true =>1; false=>0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "null" => false
+// "prince" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
