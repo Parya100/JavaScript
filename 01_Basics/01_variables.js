df169fec30e9757b5dc0b96d833a1452e208aa19
PRINCE ARYA <181065880+Parya100@users.noreply.github.com>
2025-02-05 15:17:19 +0000

    addnew1

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,17 +1,16 @@
-const accountId = 195893
-let accountEmail = "prince9070@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur";
-let accounState;
+const accountId = 144553
+let accountEmail ="parya3252@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountState;
+// accountId = 2
+ /*Prefer not to use var , becouse  in this have scope problem*/
+ 
+//  accountId = 144553
+ accountEmail ="prince233@gmail.com"
+ accountPassword ="67890"
+ accountCity ="Bhopal"
 
-//accountId = 2  // not allowed
-accountEmail = " hfbeh@.com"
-accountCity = "bengaluru"
-accountPassword = "2121212"
+ console.log(accountId);
 
-/*
-Prefer not to use var 
-because of issuse in block scope and functional scope 
-  let -  */
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity, accounState])
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
